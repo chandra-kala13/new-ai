245ddc84593fd007db4e0e0c7423a133182704d7
chandrakala <chandu99002411@gmail.com>
2026-02-09 22:48:16 +0530

    Describe your changes here

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 // test route
 app.get("/", (req, res) => {
-  res.send("chanduAI backend running with Groq 🚀");
+  res.send("chanduAI" backend running with Groq 🚀");
 });
 
 // chat route
